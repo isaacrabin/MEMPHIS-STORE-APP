95578a8a9b1b3c1c477b937e1a93ed745917b7ac
Isaac Mrongo <isaacmrongo@gmail.com>
2021-07-16 18:25:17 +0300

    initial deployment of the the angualr frontend and the nodejs backend

---
+++ backend/src/app.js
@@ -0,0 +1,56 @@
+const express= require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const app = express();
+
+const PORT = 3000;
+
+// const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+// const authRouter = require('./routes/auth');
+const orderRouter = require('./routes/orders');
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/api/orders', orderRouter)
+// app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+// app.use('/api/auth', authRouter);
+app.use('/api/orders', orderRouter);
+
+
+// var productsRouter = require('./routes/products');
+// var userRouter = require('./routes/users');
+
+//Using the routes 
+// app.use('/api/users', usersRouter);
+// app.use('/api/products', productsRouter);
+// app.use('/api/auth', authRouter);
+// app.use('/api/orders', orderRouter);
+
+
+
+
+
+/* CORS */
+app.use(cors({
+    origin: 'http:localhost:4200',
+   
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => {
+    console.log(`Serve is running at port ${PORT}`)
+})
+
+module.exports = app;
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    CartComponent,
+    FooterComponent,
+    HeaderComponent,
+    CheckoutComponent,
+    ThankYouComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    NoopAnimationsModule,
+    HttpClientModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
